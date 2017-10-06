d5b787b98c1e63df452814411a8750fe29eac418
torinlongaker@gmail.com <torinlongaker@gmail.com>
2017-10-06 10:51:06 -0700

    form changes

--- src/app/components/Form/Form.js
+++ src/app/components/Form/Form.js
@@ -2,18 +2,32 @@ import React from "react";
 import styles from "./form";
 
 export default class Form extends React.Component {
-  render(){
-    const {buttoncolor, sizing } = styles
-    return(
-      <div className={sizing}>
-        <form>
-         <label className={buttoncolor}>
-          Name:
-          <input type="text" name="name" />
+  constructor(props) {
+     super(props);
+     this.state = {value: ''};
+
+     this.handleChange = this.handleChange.bind(this);
+     this.handleSubmit = this.handleSubmit.bind(this);
+   }
+
+   handleChange(event) {
+     this.setState({value: event.target.value});
+   }
+
+   handleSubmit(event) {
+     console.log('A name was submitted: ' + this.state.value);
+     event.preventDefault();
+   }
+
+   render() {
+     return (
+       <form onSubmit={this.handleSubmit}>
+         <label>
+           Name:
+           <input type="text" value={this.state.value} onChange={this.handleChange} />
          </label>
-          <input type="submit" value="Submit" />
-        </form>
-    </div>
-    )
-  }
+         <input type="submit" value="Submit" />
+       </form>
+     );
+   }
 }
